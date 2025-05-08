1f7466c3473932f7a2211a93daffc741cd532d31
Gabriella Vilaca <mgvpp@poli.br>
2025-05-08 15:50:19 -0300

    Adiciona package.json e prepara para deploy com Next.js

---
+++ app/components/ui/card.tsx
@@ -0,0 +1,1 @@
+export function Card({ children }) { return <div className='shadow rounded p-4 bg-white'>{children}</div>; } export function CardContent({ children }) { return <div>{children}</div>; }
